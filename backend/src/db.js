b93ff35aa0f4ca1ddedc0075b6cabeb99e38da8e
David de la Puente <dpuente75marble@gmail.com>
2026-03-12 18:43:19 +0100

    fix: update sqlite path after backend refactor

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const dbPath = path.join(__dirname, "database", "ats-lti.db");
+const dbPath = path.join(__dirname, "../database", "ats-lti.db");
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
