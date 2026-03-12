21e059c1de54a2f2f7876fd9c77d5031b6a47e08
David de la Puente <dpuente75marble@gmail.com>
2026-03-12 18:45:42 +0100

    refactor: define task entity and repository contract

---
+++ backend/src/features/tasks/domain/entities/Task.js
@@ -0,0 +1,9 @@
+class Task {
+  constructor({ id, title, status }) {
+    this.id = id;
+    this.title = title;
+    this.status = status;
+  }
+}
+
+module.exports = Task;
---
+++ backend/src/features/tasks/domain/repositories/TaskRepository.js
@@ -0,0 +1,7 @@
+class TaskRepository {
+  async getAll() {
+    throw new Error('Method "getAll" must be implemented');
+  }
+}
+
+module.exports = TaskRepository;
