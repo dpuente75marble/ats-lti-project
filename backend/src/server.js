6ed47af388cf021c18e1760b730d3712f66ce35d
David de la Puente <dpuente75marble@gmail.com>
2026-03-12 18:54:57 +0100

    refactor: introduce app composition root

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const db = require("./shared/infrastructure/database/sqliteClient");
+const createApp = require("./shared/infrastructure/container/createApp");
 
-const SqliteTaskRepository = require("./features/tasks/infrastructure/repositories/SqliteTaskRepository");
-const GetTasks = require("./features/tasks/application/use-cases/GetTasks");
-const TaskController = require("./features/tasks/presentation/http/taskController");
-const createTaskRoutes = require("./features/tasks/presentation/http/taskRoutes");
-
-const app = express();
 const PORT = 3001;
-
-app.use(cors());
-app.use(express.json());
-
-const taskRepository = new SqliteTaskRepository(db);
-const getTasks = new GetTasks(taskRepository);
-const taskController = new TaskController(getTasks);
-const taskRoutes = createTaskRoutes(taskController);
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Backend ATS-LTI funcionando correctamente",
-  });
-});
-
-app.use("/api/tasks", taskRoutes);
+const app = createApp();
 
 app.listen(PORT, () => {
   console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
---
+++ backend/src/shared/infrastructure/container/createApp.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+
+const db = require("../database/sqliteClient");
+
+const SqliteTaskRepository = require("../../../features/tasks/infrastructure/repositories/SqliteTaskRepository");
+const GetTasks = require("../../../features/tasks/application/use-cases/GetTasks");
+const TaskController = require("../../../features/tasks/presentation/http/taskController");
+const createTaskRoutes = require("../../../features/tasks/presentation/http/taskRoutes");
+
+function createApp() {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  const taskRepository = new SqliteTaskRepository(db);
+  const getTasks = new GetTasks(taskRepository);
+  const taskController = new TaskController(getTasks);
+  const taskRoutes = createTaskRoutes(taskController);
+
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Backend ATS-LTI funcionando correctamente",
+    });
+  });
+
+  app.use("/api/tasks", taskRoutes);
+
+  return app;
+}
+
+module.exports = createApp;
